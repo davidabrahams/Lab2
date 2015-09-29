13543ce660f163066139ffcae4e186838b9dcef7
David Abrahams <david.abrahams@students.olin.edu>
2015-09-29 18:52:34 -0400

    added search field and action listener

--- PhotoFeed/app/src/main/java/com/mobileproto/david/photofeed/MainActivityFragment.java
+++ PhotoFeed/app/src/main/java/com/mobileproto/david/photofeed/MainActivityFragment.java
@@ -2,10 +2,14 @@ package com.mobileproto.david.photofeed;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -13,9 +17,26 @@ import android.widget.Button;
 public class MainActivityFragment extends Fragment
 {
 
+    private static final String DEBUG_TAG = "Fragment";
+
     public MainActivityFragment()
     {
     }
+
+    // The listener for the search text field
+    private TextView.OnEditorActionListener searchListener = new TextView.OnEditorActionListener()
+    {
+
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
+        {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                performSearch();
+                return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +45,15 @@ public class MainActivityFragment extends Fragment
 
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
+        EditText searchText = (EditText) view.findViewById(R.id.searchText);
+        searchText.setOnEditorActionListener(searchListener);
         return view;
+    }
+
+
+    // This function is called every time the user presses search
+    private void performSearch()
+    {
+        Log.d(DEBUG_TAG, "Search");
     }
 }
